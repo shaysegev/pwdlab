2ed3bf8df95d6fc8f4c39209a3debb7f77859fa2
Shay <shaysegev99@gmail.com>
2018-07-07 14:16:44 +0100

    Added actions folder shortname to webpack

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -5,7 +5,7 @@ module.exports = function override(config, env) {
   config.resolve.alias = {
     ...config.resolve.alias,
     Components: path.resolve(__dirname, 'src/components/'),
-    Models: path.resolve(__dirname, 'src/models/')
+    Actions: path.resolve(__dirname, 'src/actions/')
   }
 
   config.resolve.extensions = [...config.resolve.extensions, '.js']
