31677325f172d5af8e070d38f088a1a48ff900f1
Shay <shaysegev99@gmail.com>
2018-07-08 21:10:55 +0100

    Adding try/catch to auth middleware

--- backend/middleware/authenticate.js
+++ backend/middleware/authenticate.js
@@ -2,16 +2,23 @@ const User = require('./../models/user');
 
 const authenticate = async (req, res, next) => {
   const token = req.header('authorization');
+  console.log(token);
+  
 
-  const user = await User.findByToken(token);
-  console.log(user)
-  if (!user) {
-    return res.status(401).send();
+  try {
+    const user = await User.findByToken(token);
+    console.log(user)
+    if (!user) {
+      return res.status(401).send();
+    }
+  
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (e) {
+    res.status(401).send();
   }
-
-  req.user = user;
-  req.token = token;
-  next();
+  
 };
 
 module.exports = authenticate;
