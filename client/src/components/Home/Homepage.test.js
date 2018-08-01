06ae1d021bc5e3bfea98da524171cd38b9e92e4c
Shay <shaysegev99@gmail.com>
2018-08-01 21:50:38 +0100

    Importing configured store on test

--- client/src/components/Home/Homepage.test.js
+++ client/src/components/Home/Homepage.test.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import configureStore from '../../store'
+import store from '../../store'
 import { Provider } from 'react-redux'
 import AppRouter from '../../routes/AppRouter'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
-    <Provider store={configureStore()}>
+    <Provider store={store}>
       <AppRouter />
     </Provider>, 
     div
