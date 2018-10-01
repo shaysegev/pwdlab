001706c9e19a8427f024e7905ef3143c98298164
Shay <shaysegev99@gmail.com>
2018-10-01 14:23:51 +0100

    Adding API const for improving API control versioning, and updating JSDocs

--- client/src/apiConfig.js
+++ client/src/apiConfig.js
@@ -1,10 +1,24 @@
+/**
+ * @const {string} API V1 endpoint
+ */
 const apiV1 = '/api'
 
+/**
+ * @const {string} Current API endpoint
+ */
+const API = apiV1;
+
+/**
+ * @const {object} API main routes
+ */
 const apiRoutes = {
-  users: `${apiV1}/users`,
-  records: `${apiV1}/records`
+  users: `${API}/users`,
+  records: `${API}/records`
 }
 
+/**
+ * @const {object} API user routes
+ */
 const userRoutes = {
   default: apiRoutes.users,
   me: apiRoutes.users + '/me',
@@ -13,6 +27,9 @@ const userRoutes = {
   logout: apiRoutes.users + '/logout',
 }
 
+/**
+ * @const {object} API records routes
+ */
 const recordRoutes = {
   default: apiRoutes.records,
 }
